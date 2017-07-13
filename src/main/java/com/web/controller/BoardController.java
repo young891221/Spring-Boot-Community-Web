9d8582823293a0108c6d6f5d70dbbe3d508bd8a9
young891221 <young891221@gmail.com>
2017-07-13 11:20:26 +0900

    get board list

--- src/main/java/com/web/controller/BoardController.java
+++ src/main/java/com/web/controller/BoardController.java
@@ -1,6 +1,12 @@
 package com.web.controller;
 
+import com.web.service.BoardService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,13 +17,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("board")
 public class BoardController {
 
+    @Autowired
+    BoardService boardService;
+
     @GetMapping("")
     public String board() {
         return "/board/form";
     }
 
-    @GetMapping("list")
-    public String list() {
+    @GetMapping("/list")
+    public String list(@PageableDefault Pageable pageable, Model model) {
+        model.addAttribute("boardList", boardService.findBoardList(pageable));
         return "/board/list";
     }
 
---
+++ src/main/java/com/web/service/BoardService.java
@@ -0,0 +1,25 @@
+package com.web.service;
+
+import com.web.domain.Board;
+import com.web.repository.BoardRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by KimYJ on 2017-07-13.
+ */
+@Service
+public class BoardService {
+
+    @Autowired
+    BoardRepository boardRepository;
+
+    public Page<Board> findBoardList(Pageable pageable) {
+        pageable = new PageRequest(pageable.getPageNumber() <= 0 ? 0 : pageable.getPageNumber() - 1, pageable.getPageSize(), pageable.getSort());
+        return boardRepository.findAll(pageable);
+    }
+}
