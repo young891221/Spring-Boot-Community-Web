632d26ab6931a891ee7393298c87acff412e3fc1
young891221 <young891221@gmail.com>
2017-07-20 10:56:23 +0900

    .

--- src/main/java/com/web/service/BoardService.java
+++ src/main/java/com/web/service/BoardService.java
@@ -5,7 +5,6 @@ import com.web.repository.BoardRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -13,20 +12,19 @@ import org.springframework.stereotype.Service;
 /**
  * Created by KimYJ on 2017-07-13.
  */
-@Service
+@Service //서비스로 사용될 컴포넌트 정의
 public class BoardService {
 
     @Autowired
     BoardRepository boardRepository;
 
     public Page<Board> findBoardList(Pageable pageable) {
-        pageable = new PageRequest(pageable.getPageNumber() <= 0 ? 0 : pageable.getPageNumber() - 1, pageable.getPageSize());
-        Page<Board> boardPage = boardRepository.findAll(pageable);
-        return new PageImpl<>(boardPage.getContent(), pageable, boardPage.getTotalElements());
+        pageable = new PageRequest(pageable.getPageNumber() <= 0 ? 0 : pageable.getPageNumber() - 1, pageable.getPageSize()); //페이지 파라미터가 0 이하로 들어왔을때 처리 로직 / 디폴트 페이지 사이즈 사용
+        return boardRepository.findAll(pageable); //정의한 pageable을 사용하여 페이징 처리된 게시글 리스트 반환
     }
 
     public Board findBoardByIdx(Long idx) {
-        return boardRepository.findOne(idx);
+        return boardRepository.findOne(idx); //idx값을 사용하여 board 객체 반환
     }
 
     public Board saveAndUpdateBoard(Board board) {
--- src/test/java/com/web/JpaMappingTest.java
+++ src/test/java/com/web/JpaMappingTest.java
@@ -21,8 +21,8 @@ import static org.hamcrest.core.Is.is;
 /**
  * Created by KimYJ on 2017-07-12.
  */
-@RunWith(SpringRunner.class)
-@DataJpaTest
+@RunWith(SpringRunner.class) //@RunWtih 어노테이션을 사용하면 JUnit에 내장 된 러너를 사용하는 대신 어노테이션에 정의된 클래스를 호출합니다. 또한, JUnit의 확장기능을 지정하여 각각의 테스트시 독립적인 어플리케이션 컨텍스트 보장합니다.
+@DataJpaTest //스프링 부트에서 JPA 테스트위한 전영 어노테이션, 첫 설계시 엔티티간의 관계 설정 및 기능 테스트를 가능하게 도와준다. 또한, 단위 테스트가 끝날때 마다 자동 롤백을 해주어 한 층 편리한 JPA 테스트가 가능
 public class JpaMappingTest {
     private final String boardTestTitle = "테스트";
     private final String email = "test@gmail.com";
@@ -34,7 +34,7 @@ public class JpaMappingTest {
     BoardRepository boardRepository;
 
 
-    @Before
+    @Before //테스트 진행전 실행될 메소드 지정
     public void init() {
         User user = userRepository.save(User.builder()
                 .name("havi")
@@ -53,14 +53,14 @@ public class JpaMappingTest {
                 .user(user).build());
     }
 
-    @Test
+    @Test //실제 테스트가 진행될 메소드를 지정
     public void 제대로_생성_됐는지_테스트() {
-        User user = userRepository.findByEmail(email);
-        assertThat(user.getName(), is("havi"));
+        User user = userRepository.findByEmail(email); //init()에서 저장된 user의 email로 검색
+        assertThat(user.getName(), is("havi")); //각각의 필드들이 저장된 값과 실제로 일치하는지 검사
         assertThat(user.getPassword(), is("test"));
         assertThat(user.getEmail(), is(email));
 
-        Board board = boardRepository.findByTitle(boardTestTitle);
+        Board board = boardRepository.findByTitle(boardTestTitle); //init()에서 저장된 board의 테스트용 제목을 사용하여 검색
         assertThat(board.getTitle(), is(boardTestTitle));
         assertThat(board.getSubTitle(), is("서브 타이틀"));
         assertThat(board.getContent(), is("컨텐츠"));
