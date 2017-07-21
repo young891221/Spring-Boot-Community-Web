40cf377a25a58044391c6e45e9f46b4851c4fbcb
young891221 <young891221@gmail.com>
2017-07-21 16:21:36 +0900

    .

--- src/main/java/com/web/controller/BoardController.java
+++ src/main/java/com/web/controller/BoardController.java
@@ -15,20 +15,20 @@ import org.springframework.web.bind.annotation.RequestParam;
  * Created by KimYJ on 2017-07-12.
  */
 @Controller
-@RequestMapping("/board") //기본 맵핑 경로를 /board로 정의해 준다
+@RequestMapping("/board")
 public class BoardController {
 
     @Autowired
-    BoardService boardService; //boardService 의존성 주입을 위해 @Autowired 의존성을 사용하여 의존성을 찾아옵니다
+    BoardService boardService;
 
-    @GetMapping({"", "/"}) //맵핑 경로를 중괄호를 사용하여 여러개를 받을 수 있습니다
-    public String board(@RequestParam(value = "idx", defaultValue = "0") Long idx, Model model) { //@RequestParam 어노테이션을 사용하여 idx 파라미터를 필수로 받습니다. 만약 값이 없더라도 디폴트 값을 0으로 설정하여 model에 null값을 반환하도록 정의하였습니다
+    @GetMapping({"", "/"})
+    public String board(@RequestParam(value = "idx", defaultValue = "0") Long idx, Model model) {
         model.addAttribute("board", boardService.findBoardByIdx(idx));
         return "/board/form";
     }
 
     @GetMapping("/list")
-    public String list(@PageableDefault Pageable pageable, Model model) { //@PageableDefault 어노테이션의 파라미터인 size, sort, direction 등을 사용하여 페이징 처리에 대한 규약을 정의할 수 있습니다
+    public String list(@PageableDefault Pageable pageable, Model model) {
         model.addAttribute("boardList", boardService.findBoardList(pageable));
         return "/board/list";
     }
--- src/main/java/com/web/service/BoardService.java
+++ src/main/java/com/web/service/BoardService.java
@@ -12,19 +12,19 @@ import org.springframework.stereotype.Service;
 /**
  * Created by KimYJ on 2017-07-13.
  */
-@Service //서비스로 사용될 컴포넌트 정의
+@Service
 public class BoardService {
 
     @Autowired
     BoardRepository boardRepository;
 
     public Page<Board> findBoardList(Pageable pageable) {
-        pageable = new PageRequest(pageable.getPageNumber() <= 0 ? 0 : pageable.getPageNumber() - 1, pageable.getPageSize()); //페이지 파라미터가 0 이하로 들어왔을때 처리 로직 / 디폴트 페이지 사이즈 사용
-        return boardRepository.findAll(pageable); //정의한 pageable을 사용하여 페이징 처리된 게시글 리스트 반환
+        pageable = new PageRequest(pageable.getPageNumber() <= 0 ? 0 : pageable.getPageNumber() - 1, pageable.getPageSize());
+        return boardRepository.findAll(pageable);
     }
 
     public Board findBoardByIdx(Long idx) {
-        return boardRepository.findOne(idx); //idx값을 사용하여 board 객체 반환
+        return boardRepository.findOne(idx);
     }
 
     public Board saveAndUpdateBoard(Board board) {
