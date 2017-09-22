dbeb9e8fe4ebc5decdb07014004a3814c5f273c9
young891221 <young891221@gmail.com>
2017-09-22 16:13:01 +0900

    init SocialUser annotation

---
+++ src/main/java/com/web/resolver/UserArgumentResolver.java
@@ -0,0 +1,99 @@
+package com.web.resolver;
+
+import com.web.annotation.SocialUser;
+import com.web.domain.User;
+import com.web.domain.enums.SocialType;
+import com.web.repository.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.MethodParameter;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
+import static com.web.domain.enums.SocialType.FACEBOOK;
+import static com.web.domain.enums.SocialType.GOOGLE;
+import static com.web.domain.enums.SocialType.KAKAO;
+
+@Component
+public class UserArgumentResolver implements HandlerMethodArgumentResolver {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.getParameterAnnotation(SocialUser.class) != null && parameter.getParameterType().equals(User.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        HttpSession session = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest().getSession();
+        User user = (User) session.getAttribute("user");
+        return getUser(user);
+    }
+
+    private User getUser(User user) {
+        if(user == null) {
+            try {
+                OAuth2Authentication authentication = (OAuth2Authentication) SecurityContextHolder.getContext().getAuthentication();
+                Map<String, String> map = (HashMap<String, String>) authentication.getUserAuthentication().getDetails();
+                user = userRepository.findByEmail(map.get("email"));
+                if (user == null) { user = userRepository.save(convertUser(String.valueOf(authentication.getAuthorities().toArray()[0]), map)); }
+                setRoleIfNotSame(user, authentication, map);
+            } catch (ClassCastException e) {
+                return user;
+            }
+        }
+        return user;
+    }
+
+    private User convertUser(String authority, Map<String, String> map) {
+        if(FACEBOOK.isEquals(authority)) return getModernUser(FACEBOOK, map);
+        else if(GOOGLE.isEquals(authority)) return getModernUser(GOOGLE, map);
+        else if(KAKAO.isEquals(authority)) return getKaKaoUser(map);
+        return null;
+    }
+
+    private User getModernUser(SocialType socialType, Map<String, String> map) {
+        return User.builder()
+                .name(map.get("name"))
+                .email(map.get("email"))
+                .pincipal(map.get("id"))
+                .socialType(socialType)
+                .createdDate(LocalDateTime.now())
+                .build();
+    }
+
+    private User getKaKaoUser(Map<String, String> map) {
+        HashMap<String, String> propertyMap = (HashMap<String, String>)(Object) map.get("properties");
+        return User.builder()
+                .name(propertyMap.get("nickname"))
+                .email(map.get("kaccount_email"))
+                .pincipal(String.valueOf(map.get("id")))
+                .socialType(KAKAO)
+                .createdDate(LocalDateTime.now())
+                .build();
+    }
+
+    private void setRoleIfNotSame(User user, OAuth2Authentication authentication, Map<String, String> map) {
+        if(!authentication.getAuthorities().contains(new SimpleGrantedAuthority(user.getSocialType().getValue()))) {
+            SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken(map, "N/A", AuthorityUtils.createAuthorityList(user.getSocialType().getValue())));
+        }
+    }
+}
