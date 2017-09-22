2fe21ba372369aa8a2ea47d6690d50f304db7f74
young891221 <young891221@gmail.com>
2017-09-22 17:13:54 +0900

    .

--- src/main/java/com/web/BootWebApplication.java
+++ src/main/java/com/web/BootWebApplication.java
@@ -5,20 +5,33 @@ import com.web.domain.User;
 import com.web.domain.enums.BoardType;
 import com.web.repository.BoardRepository;
 import com.web.repository.UserRepository;
+import com.web.resolver.UserArgumentResolver;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.stream.IntStream;
 
 @SpringBootApplication
-public class BootWebApplication {
+public class BootWebApplication extends WebMvcConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BootWebApplication.class, args);
+	}
+
+	@Autowired
+	private UserArgumentResolver userArgumentResolver;
+
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+		argumentResolvers.add(userArgumentResolver);
 	}
 
 	@Bean
--- src/main/java/com/web/controller/LoginController.java
+++ src/main/java/com/web/controller/LoginController.java
@@ -1,8 +1,12 @@
 package com.web.controller;
 
+import com.web.annotation.SocialUser;
+import com.web.domain.User;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * Created by KimYJ on 2017-09-13.
@@ -11,8 +15,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class LoginController {
 
     @GetMapping("/login")
-    public String login(@RequestParam(value = "error", required = false) String error) {
+    public String login() {
         return "/login/login";
     }
 
+    @GetMapping(value = "/{facebook|google|kakao}/complete")
+    public String facebookComplete(@SocialUser User user, HttpSession session) {
+        session.setAttribute("user", user);
+        return "/login/complete";
+    }
 }
