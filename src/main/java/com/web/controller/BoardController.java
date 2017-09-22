4bb998cb118b04ee3b6aca1ad782dfd8e4bc8cf5
young891221 <young891221@gmail.com>
2017-09-22 18:09:46 +0900

    layout

--- src/main/java/com/web/controller/BoardController.java
+++ src/main/java/com/web/controller/BoardController.java
@@ -2,7 +2,6 @@ package com.web.controller;
 
 import com.web.service.BoardService;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
@@ -17,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequestMapping("/board")
 public class BoardController {
-    //@See https://docs.spring.io/spring-boot/docs/current/reference/html/using-boot-spring-beans-and-dependency-injection.html
     private final BoardService boardService;
 
     public BoardController(BoardService boardService) {
