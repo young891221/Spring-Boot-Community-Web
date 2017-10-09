5e372b63dd5048284cfa67429c42a9a4153b92c0
young891221 <young891221@gmail.com>
2017-10-09 11:45:57 +0900

    .

--- src/main/java/com/web/controller/AuthorityTestController.java
+++ src/main/java/com/web/controller/AuthorityTestController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class RoleTestController {
+public class AuthorityTestController {
 
     @GetMapping("/facebook")
     public String facebook() {
