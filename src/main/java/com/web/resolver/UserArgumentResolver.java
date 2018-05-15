6935b8d1c6bae5f82b87b1e0e8510af30d26fbc8
young891221 <young891221@gmail.com>
2018-05-15 15:22:31 +0900

    .

--- src/main/java/com/web/resolver/UserArgumentResolver.java
+++ src/main/java/com/web/resolver/UserArgumentResolver.java
@@ -5,23 +5,14 @@ import com.web.domain.User;
 import com.web.domain.enums.SocialType;
 import com.web.repository.UserRepository;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.MethodParameter;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.support.WebDataBinderFactory;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
@@ -41,11 +32,11 @@ import static com.web.domain.enums.SocialType.KAKAO;
 @Component
 public class UserArgumentResolver implements HandlerMethodArgumentResolver {
 
-    @Autowired
     private UserRepository userRepository;
 
-    @Autowired
-    private OAuth2AuthorizedClientService authorizedClientService;
+    public UserArgumentResolver(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
@@ -63,11 +54,12 @@ public class UserArgumentResolver implements HandlerMethodArgumentResolver {
         if(user == null) {
             try {
                 OAuth2AuthenticationToken authentication = (OAuth2AuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
-                Map<String, String> map = getUserInfo(authentication);
-
+                Map<String, Object> map = authentication.getPrincipal().getAttributes();
                 User convertUser = convertUser(authentication.getAuthorizedClientRegistrationId(), map);
+
                 user = userRepository.findByEmail(convertUser.getEmail());
                 if (user == null) { user = userRepository.save(convertUser); }
+
                 setRoleIfNotSame(user, authentication, map);
                 session.setAttribute("user", user);
             } catch (ClassCastException e) {
@@ -77,55 +69,35 @@ public class UserArgumentResolver implements HandlerMethodArgumentResolver {
         return user;
     }
 
-    private Map<String, String> getUserInfo(OAuth2AuthenticationToken authentication) {
-        OAuth2AuthorizedClient client = authorizedClientService.loadAuthorizedClient(authentication.getAuthorizedClientRegistrationId(), authentication.getName());
-
-        String userInfoEndpointUri = client.getClientRegistration()
-                .getProviderDetails()
-                .getUserInfoEndpoint()
-                .getUri();
-
-        Map<String, String> map = new HashMap<>();
-
-        if (!StringUtils.isEmpty(userInfoEndpointUri)) {
-            HttpHeaders headers = new HttpHeaders();
-            headers.add(HttpHeaders.AUTHORIZATION, "Bearer " + client.getAccessToken().getTokenValue());
-
-            ResponseEntity<Map> response = new RestTemplate().exchange(userInfoEndpointUri, HttpMethod.GET, new HttpEntity<>("", headers), Map.class);
-            map = response.getBody();
-        }
-        return map;
-    }
-
-    private User convertUser(String authority, Map<String, String> map) {
+    private User convertUser(String authority, Map<String, Object> map) {
         if(FACEBOOK.isEquals(authority)) return getModernUser(FACEBOOK, map);
         else if(GOOGLE.isEquals(authority)) return getModernUser(GOOGLE, map);
         else if(KAKAO.isEquals(authority)) return getKaKaoUser(map);
         return null;
     }
 
-    private User getModernUser(SocialType socialType, Map<String, String> map) {
+    private User getModernUser(SocialType socialType, Map<String, Object> map) {
         return User.builder()
-                .name(map.get("name"))
-                .email(map.get("email"))
-                .pincipal(map.get("id"))
+                .name(String.valueOf(map.get("name")))
+                .email(String.valueOf(map.get("email")))
+                .pincipal(String.valueOf(map.get("id")))
                 .socialType(socialType)
                 .createdDate(LocalDateTime.now())
                 .build();
     }
 
-    private User getKaKaoUser(Map<String, String> map) {
-        HashMap<String, String> propertyMap = (HashMap<String, String>)(Object) map.get("properties");
+    private User getKaKaoUser(Map<String, Object> map) {
+        Map<String, String> propertyMap = (HashMap<String, String>) map.get("properties");
         return User.builder()
                 .name(propertyMap.get("nickname"))
-                .email(map.get("kaccount_email"))
+                .email(String.valueOf(map.get("kaccount_email")))
                 .pincipal(String.valueOf(map.get("id")))
                 .socialType(KAKAO)
                 .createdDate(LocalDateTime.now())
                 .build();
     }
 
-    private void setRoleIfNotSame(User user, OAuth2AuthenticationToken authentication, Map<String, String> map) {
+    private void setRoleIfNotSame(User user, OAuth2AuthenticationToken authentication, Map<String, Object> map) {
         if(!authentication.getAuthorities().contains(new SimpleGrantedAuthority(user.getSocialType().getRoleType()))) {
             SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken(map, "N/A", AuthorityUtils.createAuthorityList(user.getSocialType().getRoleType())));
         }
